c558ce231a52c6cb599a142b947db78e0bdd76c6
Damian <damianmdst@gmail.com>
2019-04-24 18:02:54 -0300

    Add facebook login and custom evento with native module for android

--- src/components/FbLoginButton/FbLoginButton.js
+++ src/components/FbLoginButton/FbLoginButton.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import { LoginButton, AccessToken, AppEventsLogger } from 'react-native-fbsdk';
+('');
 
 export default class FbLoginButton extends Component {
   render() {
     return (
       <View>
         <LoginButton
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                console.log("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                console.log("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    console.log(data.accessToken.toString())
-                  }
-                )
-              }
+          onLoginFinished={(error, result) => {
+            if (error) {
+              console.log('login has error: ' + result.error);
+            } else if (result.isCancelled) {
+              console.log('login is cancelled.');
+            } else {
+              AppEventsLogger.logEvent('loggedIn');
+              AccessToken.getCurrentAccessToken().then(data => {
+                console.log(data.accessToken.toString());
+              });
             }
-          }
-          onLogoutFinished={() => console.log("logout.")}/>
+          }}
+          onLogoutFinished={() => console.log('logout.')}
+        />
       </View>
     );
   }
-};
+}
